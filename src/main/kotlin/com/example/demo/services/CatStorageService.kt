961406fd73d4b7daa697d8e4cfae1b91b7d1d2d0
Maxim Vasilyuk <maxim1.480.86@gmail.com>
2023-01-03 21:15:02 +0300

    Добавлен новый кит

---
+++ src/main/kotlin/com/example/demo/services/CatStorageService.kt
@@ -0,0 +1,35 @@
+package com.example.demo.services
+
+import com.example.demo.models.CatModel
+
+class CatStorageService(
+    private val officialCats: Set<String>,
+) {
+    fun getCats(): List<CatModel> {
+        return mutableListOf(
+            CatModel(
+                id = 1,
+                name = "Борис",
+                url = "cat_1",
+                isOfficial = officialCats.contains("Борис"),
+                downloadCount = 12
+            ),
+
+            CatModel(
+                id = 2,
+                name = "Дуся",
+                url = "cat_2",
+                isOfficial = officialCats.contains("Дуся"),
+                downloadCount = 50
+            ),
+
+            CatModel(
+                id = 3,
+                name = "Анфис",
+                url = "cat_3",
+                isOfficial = officialCats.contains("Анфис"),
+                downloadCount = 100
+            )
+        )
+    }
+}
